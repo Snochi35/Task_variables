13247b7b447b46b12155d1fb58525a7a9e9bdc78
Snochi35 <levanianoize@gmail.com>
2024-09-02 16:00:55 +0300

    Add files via upload

---
+++ Task_Variables.py
@@ -0,0 +1,5 @@
+number_of_comleted_tasks = 12
+hours_spent = 1.5
+cours_name = "Python"
+print('Курс:',':',cours_name,',','всего задач'',',number_of_comleted_tasks,',','затрачено часов'':',hours_spent,',''среднее время выполнения',(hours_spent/number_of_comleted_tasks))
+# =D
